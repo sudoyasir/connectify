9d8696b7a511fdc7b4614e08c02d5f1c5e80fc70
Yasir Nawaz <y451rmahar@gmail.com>
2024-07-23 17:03:44 +0500

    :package: add clerk authentication

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const page = () => {
+const Upcoming = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <h1 className="text-3xl font-bold">Upcomings</h1>
     </section>
   );
 };
 
-export default page;
+export default Upcoming;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,25 +1,25 @@
 "use client";
-
-import React from "react";
-import { sidebarLinks } from "@/constants";
-
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 
 const Sidebar = () => {
   const pathname = usePathname();
+
   return (
-    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[246px]">
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
-        {sidebarLinks.map((link) => {
+        {sidebarLinks.map((item) => {
           const isActive =
-            pathname === link.route || pathname.startsWith(link.route);
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+
           return (
             <Link
-              href={link.route}
-              key={link.label}
+              href={item.route}
+              key={item.label}
               className={cn(
                 "flex gap-4 items-center p-4 rounded-lg justify-start",
                 {
@@ -28,13 +28,13 @@ const Sidebar = () => {
               )}
             >
               <Image
-                src={link.imgUrl}
-                alt={link.label}
+                src={item.imgUrl}
+                alt={item.label}
                 width={24}
                 height={24}
               />
               <p className="text-lg font-semibold max-lg:hidden">
-                {link.label}
+                {item.label}
               </p>
             </Link>
           );

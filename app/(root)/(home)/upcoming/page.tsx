3e0075dc41942dc0361a6dc416d26da3e852666b
Yasir Nawaz <y451rmahar@gmail.com>
2024-07-24 14:58:06 +0500

    :package: add upcoming meetings page

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -5,7 +5,7 @@ const Upcoming = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Upcomings</h1>
-      <CallList />
+      <CallList type="upcoming" />
     </section>
   );
 };
